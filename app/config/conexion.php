65c5e5a895a5b2a1b232036188fc56dee45ffd17
Riudux <richifrias54@gmail.com>
2026-04-17 18:20:48 -0600

    conexion
    Hice la modificacion de conexion ya que mi base de datos donde manejo todo no uso contraseña pero donde esta desplegado si usa

--- app/config/conexion.php
+++ app/config/conexion.php
@@ -14,6 +14,8 @@
     $pass = '';          // La contraseña de ese usuario. Por defecto, XAMPP no le pone contraseña a 'root', por eso la dejamos vacía.
     $db = 'vitalconnection'; // El nombre asignado a la base de datos que creamos en nuestro gestor (como phpMyAdmin).
 
+    $passvm = 'server1'; // Usuario para conexión remota desde Visual Studio Code (VSCode).
+
     // 2. Ejecutar la conexión a la base de datos.
     // 'new mysqli' es un comando nativo de PHP que abre una nueva conexión con el servidor MySQL.
     // Le pasamos las 4 variables que acabamos de definir en el orden exacto que requiere PHP.
@@ -22,11 +24,9 @@
     // 3. Comprobar si la conexión fue exitosa.
     // Evaluamos una propiedad llamada "connect_error" que guarda el motivo del error si algo falla.
     if ($conn->connect_error) {
-        // Si hay un error (ej. nos equivocamos en la contraseña o el MySQL está apagado),
-        // se ejecuta la instrucción 'die()'. 'die()' mata o detiene por completo 
-        // la página en este instante, mostrando el texto de error. Se usa para evitar 
-        // que el código trate de seguir cargando datos si no hay donde extraerlos.
-        die('Error de conexión: ' . $conn->connect_error);
+        $conn = new mysqli($name, $user, $passvm, $db);
+    }else {
+        die("Conexión fallida: " . $conn->connect_error);
     }
     // Si el código llega hasta aquí significa que la conexión fue exitosa.
     // A partir de ahora, cualquier archivo que incluya 'conexion.php' podrá usar la variable
